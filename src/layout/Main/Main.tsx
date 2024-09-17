ed0b69f2b9240d3112ef6bdc9c04ff819b9b82f1
Patrick Da Silva <patrick1dasilva@gmail.com>
2024-09-18 00:00:17 +0300

    Removed toy lines

--- src/layout/Main/Main.tsx
+++ src/layout/Main/Main.tsx
@@ -1,12 +1,12 @@
 import "./Main.css"
 
-const range = (n: number) => Array.from({ length: n }, (_, i) => i)
+// const range = (n: number) => Array.from({ length: n }, (_, i) => i)
 
 const Main = () => (
 	<main>
-		{range(100).map((index: number) => (
+		{/* {range(100).map((index: number) => (
 			<p key={index}>Line number {index + 1}</p>
-		))}
+		))} */}
 	</main>
 )
 export default Main
