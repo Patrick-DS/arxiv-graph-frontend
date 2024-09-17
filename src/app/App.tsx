6f155f7b63a9d8c71476dd619fce35253cf1dc97
Patrick Da Silva <patrick1dasilva@gmail.com>
2024-09-17 23:59:38 +0300

    Initial setup of CSS layout

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,12 @@
 import "./App.css"
+import { Main, Header, Footer } from "../layout"
 
-const App = () => <main></main>
+const App = () => (
+	<>
+		<Header />
+		<Main />
+		<Footer />
+	</>
+)
 
 export default App
---
+++ src/layout/Footer/Footer.tsx
@@ -0,0 +1,6 @@
+import { FC } from "react"
+import "./Footer.css"
+
+const Footer: FC = () => <footer></footer>
+
+export default Footer
